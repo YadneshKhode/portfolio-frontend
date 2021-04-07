3dadefd2c4b856651d218823303c163e1efeba41
Yadnesh Khode <yadneshkhode@gmail.com>
2021-04-07 22:42:14 +0530

    updated resume link

--- src/data/config.js
+++ src/data/config.js
@@ -6,5 +6,5 @@ module.exports = {
   greetingDescription: "I'm Yadnesh Khode and I'm a Software Engineer!",
   githubUrl: "https://github.com/yadneshkhode",
   linkedinUrl: "https://www.linkedin.com/in/yadnesh-khode-4a89b3194/",
-  cvLink: "https://drive.google.com/file/d/1UBQlqLrkzsoSBpsIjV-8Of3rtbg6U8mC/view?usp=sharing",
+  cvLink: "https://drive.google.com/file/d/1PTxpJigfy9ob_7zHzJSuc_LWYhf9HOq3/view?usp=sharing",
 };
