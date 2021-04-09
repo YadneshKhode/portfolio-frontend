29b0fcefd5deddd8a905bd8d9395026176197693
Yadnesh Khode <yadneshkhode@gmail.com>
2021-04-09 20:25:24 +0530

    changed URLS

--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,9 @@
 
 module.exports = {
-  // TODO: change your profile information here
   name: "Yadnesh Khode",
   greeting: "Hey 👋",
   greetingDescription: "I'm Yadnesh Khode and I'm a Software Engineer!",
   githubUrl: "https://github.com/yadneshkhode",
-  linkedinUrl: "https://www.linkedin.com/in/yadnesh-khode-4a89b3194/",
-  cvLink: "https://drive.google.com/file/d/1PTxpJigfy9ob_7zHzJSuc_LWYhf9HOq3/view?usp=sharing",
+  linkedinUrl: "www.linkedin.com/in/yadnesh-khode",
+  cvLink: "https://drive.google.com/file/d/1ITyreh9U1E6e4ADU90blxtmDL-wykzcM/view?usp=sharing",
 };
